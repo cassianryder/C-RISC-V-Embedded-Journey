da29593a07e828ef74e006c22709040f38f3a0c6
cassianryder <caoy7571@gmail.com>
2026-04-17 21:41:34 +0800

    (01-basics):池塘水质监控（溶氧，水温）闭环迭代两轮，c primer配套练习

--- exercises/01-basics/11.c
+++ exercises/01-basics/11.c
@@ -20,23 +20,68 @@
 
 //池塘数据监控器
 //输入温度和溶氧，输出温度正常/异常，溶氧正常/不足
+// #include <stdio.h>
+//
+// void check_oxygen(double oxygen)
+// {
+//  if(oxygen < 3.65) printf("溶氧异常\n");
+//   else printf("溶氧正常\n");
+// }
+//
+// void check_temperature(double oxygen,double temperature)
+// {
+//  if(temperature < 23.45 || temperature > 28.76) printf("水温异常\n");
+//   else printf("水温正常\n");
+//   check_oxygen(oxygen);//嵌套调用 
+// }
+// int main(void){
+//   double oxygen,temperature;//局部变量
+//   if (scanf("%lf%lf",&oxygen,&temperature) != 2)//scanf读取返回值判断，\0 空格
+//   {
+//     printf("无效值！\n");
+//     return 1;
+//   }
+//   printf("溶氧：%.2f mg/L\n",oxygen);
+//   printf("水温：%.2f C\n",temperature);
+//   check_temperature(oxygen,temperature);//实际参数
+//   return 0;
+// }
+
+//C Primer plus 
+// #include <stdio.h>
+// #define SIZE 50
+//
+//
+// int main(void){
+//  float list[SIZE];
+//  readlist(list,SIZE);
+//  sort(list,SIZE);
+//   average(list,SIZE);
+//   bargraph(list,SIZE);
+//   return 0;
+// }
+
+//9.1 lethead1.c 
 #include <stdio.h>
+#define NAME "GIGATHINK, INC."
+#define ADDRESS "101 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define WIDTH 40
 
-void check_oxygen(double oxygen)
+void starbar(void)
 {
- if(oxygen < 3.00) printf("溶氧异常\n");
-  else printf("溶氧正常\n");
+    for(int count = 1;count < WIDTH;++count)
+  {
+    putchar('*');
+    putchar('\n');
+  }
 }
 
-void check_temperature(double oxygen,double temperature)
-{
- if(temperature > 28.00) printf("温度异常\n");
-  else printf("温度正常\n");
-}
 int main(void){
-  double m,n;
-  scanf("%lf%lf",&m,&n);
-  check_oxygen(m);
-  check_temperature(m,n);
-  return 0;
+  starbar();
+printf("%s\n", NAME);
+printf("%s\n", ADDRESS);
+printf("%s\n", PLACE);
+  starbar();
 }
+
